754e883fc4ef2a3069eb31ff184b1b6c3ef8727f
beepscore <support@beepscore.com>
2015-01-29 02:27:21 -0800

    Add puzzle3.py with comment and TODO for possible solution.

---
+++ puzzle3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import urllib.request
+
+"""
+Reference
+puzzle3 url
+http://www.pythonchallenge.com/pc/def/equality.html
+
+puzzle3 url comes from solution to puzzle2
+test_puzzle2.py
+
+One small letter, surrounded by
+EXACTLY three big bodyguards on each of its sides.
+
+In browser, view page source
+
+TODO: In long string at bottom, look for one lowercase letter
+surrounded by 3 uppercase each side.
+Could use regular expression or some match function.
+
+"""
