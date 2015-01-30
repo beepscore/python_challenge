f61a5e783f7136a2bd1d3cd200a5c43c9dc32ac8
beepscore <support@beepscore.com>
2015-01-29 19:38:53 -0800

    Add puzzle3_response_expected.txt and test_puzzle3.py

---
+++ test/test_puzzle3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import puzzle3
+import unittest
+import file_utils
+import url_utils
+
+
+class Testpuzzle3(unittest.TestCase):
+    """ Use test file puzzle3_response_expected.txt in place of web response.
+        This reduces test scope and makes tests faster.
+    """
+
+    def setUp(self):
+        self.test_file_utils = file_utils.FileUtils()
+        self.test_url_utils = url_utils.UrlUtils()
+        self.leader = "<!--\n"
+        self.trailer = "\n-->\n"
+
+    def test_get_mess_and_trailer(self):
+        content = self.test_file_utils.get_file_string(
+            "test/puzzle3_response_expected.txt")
+        actual = self.test_url_utils.get_mess_and_trailer(content, self.leader)
+        self.assertEqual("kAewtloYgcFQ", actual[:12])
+
+    def test_get_mess(self):
+        content = self.test_file_utils.get_file_string(
+            "test/puzzle3_response_expected.txt")
+        actual = self.test_url_utils.get_mess(
+            content, self.leader, self.trailer)
+        self.assertEqual(101249, len(actual))
+        # test start
+        self.assertEqual("kAewtloYgcFQ", actual[:12])
+        # test end
+        self.assertEqual("JkKbtSipiqBd", actual[len(actual)-12:])
+
+
+if __name__ == "__main__":
+    unittest.main()
