4193a43a677ff4ca44efc238a1792bd2401dbb95
beepscore <support@beepscore.com>
2015-01-31 00:26:02 -0800

    In puzzle2.py add class Puzzle2. Update test_puzzle2.py

--- puzzle2.py
+++ puzzle2.py
@@ -1,42 +1,42 @@
 #!/usr/bin/env python3
 
-"""
-Reference
-puzzle2 url
-http://www.pythonchallenge.com/pc/def/ocr.html
-
-puzzle2 url comes from solution to puzzle1
-test_puzzle1.py
-
-In browser, view page source
-find rare characters in the mess below:
-
-I put the string in a test file.
-"""
-
-
-def get_character_counts(a_string):
-    """ return a list of lists.
-    This maintains character order
-    http://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value?rq=1
-    http://stackoverflow.com/questions/15733558/python-ordereddict-not-keeping-element-order?rq=1
+class Puzzle2:
     """
-    character_counts = []
-    characters = []
-    for char in a_string:
-        # list comprehension
-        characters = [item[0] for item in character_counts]
-        if char not in characters:
-            character_counts.append([char, 1])
-        else:
-            for item in character_counts:
-                if item[0] == char:
-                    item[1] += 1
-                    break
-    return character_counts
+    Reference
+    puzzle2 url
+    http://www.pythonchallenge.com/pc/def/ocr.html
 
+    puzzle2 url comes from solution to puzzle1
+    test_puzzle1.py
 
-def get_string_from_unique_chars(a_string):
-    character_counts = get_character_counts(a_string)
-    unique_chars = list(char for char, count in character_counts if count == 1)
-    return ''.join(unique_chars)
+    In browser, view page source
+    find rare characters in the mess below:
+
+    I put the string in a test file.
+    """
+
+    def get_character_counts(self, a_string):
+        """ return a list of lists.
+        This maintains character order
+        http://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value?rq=1
+        http://stackoverflow.com/questions/15733558/python-ordereddict-not-keeping-element-order?rq=1
+        """
+        character_counts = []
+        characters = []
+        for char in a_string:
+            # list comprehension
+            characters = [item[0] for item in character_counts]
+            if char not in characters:
+                character_counts.append([char, 1])
+            else:
+                for item in character_counts:
+                    if item[0] == char:
+                        item[1] += 1
+                        break
+        return character_counts
+
+    def get_string_from_unique_chars(self, a_string):
+        character_counts = self.get_character_counts(a_string)
+        unique_chars = list(char for char, count in character_counts
+                            if count == 1)
+        return ''.join(unique_chars)
--- test/test_puzzle2.py
+++ test/test_puzzle2.py
@@ -16,12 +16,13 @@ class TestPuzzle2(unittest.TestCase):
         self.test_url_utils = url_utils.UrlUtils()
         self.leader = "mess below:\n-->\n\n<!--\n"
         self.trailer = "\n-->"
+        self.puzzle2 = puzzle2.Puzzle2()
 
     def test_get_character_counts(self):
         content = self.test_file_utils.get_file_string(
             "test/puzzle2_response_expected.txt")
         mess = self.test_url_utils.get_mess(content, self.leader, self.trailer)
-        character_counts = puzzle2.get_character_counts(mess)
+        character_counts = self.puzzle2.get_character_counts(mess)
         self.assertEqual(25, len(character_counts))
 
     def test_get_string_from_unique_chars(self):
@@ -30,7 +31,8 @@ class TestPuzzle2(unittest.TestCase):
         mess = self.test_url_utils.get_mess(content, self.leader, self.trailer)
         # next puzzle puzzle3 url is at 'equality'
         # http://www.pythonchallenge.com/pc/def/equality.html
-        self.assertEqual("equality", puzzle2.get_string_from_unique_chars(mess))
+        self.assertEqual("equality",
+                         self.puzzle2.get_string_from_unique_chars(mess))
 
 
 if __name__ == "__main__":
