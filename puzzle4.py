32d4c6b0228208dd5bf516771b6c6bbc693073d6
beepscore <support@beepscore.com>
2015-04-04 02:41:41 -0700

    In puzzle4.py revise methods to handle response special cases such as no ending digits or divide by two.

--- puzzle4.py
+++ puzzle4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import url_utils
+import re
 
 
 class Puzzle4:
@@ -32,26 +33,44 @@ class Puzzle4:
         url = url_start
         for i in range(count_max):
             url = self.get_next_url(url)
-            print("url: " + url)
+            print("i: {0:d} url: {1}".format(i, url))
+            if url == "http://www.pythonchallenge.com/pc/def/peak.html":
+                break
+
         return url
 
     def get_next_url(self, url):
         text = self.get_text_from_url(url)
-        nothing_digits = self.get_nothing_digits_from_text(text)
-        next_url = self.get_url_from_nothing_digits(nothing_digits)
-        return next_url
+
+        if text == "Yes. Divide by two and keep going.":
+            # NOTE: sic. divide digits in url
+            double_next_url_digits = self.get_next_url_digits_from_text(url)
+            double_next_url_value = int(double_next_url_digits)
+            next_url_value = double_next_url_value // 2
+            next_url_digits = str(next_url_value)
+            return self.get_next_url_from_text(next_url_digits)
+        else:
+            return self.get_next_url_from_text(text)
 
     def get_text_from_url(self, url):
         response_bytes = self.url_utils.get_response(url)
         text = self.url_utils.string_from_bytes(response_bytes)
         return text
 
-    def get_nothing_digits_from_text(self, text):
-        text_start = "and the next nothing is "
-        nothing_digits = text[len(text_start):]
-        return nothing_digits
+    def get_next_url_from_text(self, text):
+        next_url_digits = self.get_next_url_digits_from_text(text)
+        if next_url_digits == "":
+            # TODO: consider percent escaping text to make method more general
+            return "http://www.pythonchallenge.com/pc/def/" + text
+        else:
+            url_start = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+            return url_start + next_url_digits
 
-    def get_url_from_nothing_digits(self, nothing_digits):
-        url_start = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
-        url = url_start + nothing_digits
-        return url
+    def get_next_url_digits_from_text(self, text):
+        # one or more digits followed by end of line
+        regex = r'\d+$'
+        matches = re.findall(regex, text)
+        if len(matches) > 0:
+            return matches[0]
+        else:
+            return ""
--- test/test_puzzle4.py
+++ test/test_puzzle4.py
@@ -21,25 +21,77 @@ class TestPuzzle4(unittest.TestCase):
         expected = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=45439"
         self.assertEqual(expected, url3)
 
+    def test_get_next_url_from_16044(self):
+        url1 = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=16044"
+        url2 = self.puzzle4.get_next_url(url1)
+        expected = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=8022"
+        self.assertEqual(expected, url2)
+
     def test_get_text_from_url(self):
-        url1 = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+        url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
         expected = "and the next nothing is 44827"
-        self.assertEqual(expected, self.puzzle4.get_text_from_url(url1))
+        self.assertEqual(expected, self.puzzle4.get_text_from_url(url))
 
-    def test_get_nothing_digits_from_text(self):
+    def test_get_text_from_url_tired(self):
+        url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=45439"
+        expected = "<font color=red>Your hands are getting tired </font>and the next nothing is 94485"
+        self.assertEqual(expected, self.puzzle4.get_text_from_url(url))
+
+    def test_get_text_from_url_divide(self):
+        url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=16044"
+        expected = "Yes. Divide by two and keep going."
+        self.assertEqual(expected, self.puzzle4.get_text_from_url(url))
+
+    def test_get_text_from_url_peak(self):
+        url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=66831"
+        expected = "peak.html"
+        self.assertEqual(expected, self.puzzle4.get_text_from_url(url))
+
+    def test_get_next_url_from_text(self):
         text = "and the next nothing is 44827"
-        expected = "44827"
-        self.assertEqual(expected, self.puzzle4.get_nothing_digits_from_text(text))
-
-    def test_get_url_from_nothing_digits(self):
-        nothing_digits = "44827"
         expected = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=44827"
-        self.assertEqual(expected, self.puzzle4.get_url_from_nothing_digits(nothing_digits))
+        self.assertEqual(expected, self.puzzle4.get_next_url_from_text(text))
 
-    def test_get_url_for_url_start_count_max(self):
+    def test_get_next_url_from_text_tired(self):
+        text  = "<font color=red>Your hands are getting tired </font>and the next nothing is 94485"
+        expected = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=94485"
+        self.assertEqual(expected, self.puzzle4.get_next_url_from_text(text))
+
+    def test_get_next_url_from_text_peak(self):
+        text = "peak.html"
+        expected = "http://www.pythonchallenge.com/pc/def/peak.html"
+        self.assertEqual(expected, self.puzzle4.get_next_url_from_text(text))
+
+    def test_get_next_url_from_text_no_digits(self):
+        text = "foo not percent escaped"
+        expected = "http://www.pythonchallenge.com/pc/def/foo not percent escaped"
+        self.assertEqual(expected, self.puzzle4.get_next_url_from_text(text))
+
+    def test_get_next_url_digits_from_text_tired(self):
+        text  = "<font color=red>Your hands are getting tired </font>and the next nothing is 94485"
+        expected = "94485"
+        self.assertEqual(expected, self.puzzle4.get_next_url_digits_from_text(text))
+
+    def test_get_next_url_digits_from_text_peak(self):
+        text = "peak.html"
+        expected = ""
+        self.assertEqual(expected, self.puzzle4.get_next_url_digits_from_text(text))
+
+    def test_get_next_url_digits_from_text_no_digits_at_end(self):
+        text = "123 not at end of string"
+        expected = ""
+        self.assertEqual(expected, self.puzzle4.get_next_url_digits_from_text(text))
+
+    def test_get_url_for_url_start_count_max_2(self):
         url1 = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
         url_end = self.puzzle4.get_url_for_url_start_count_max(url1, 2)
         expected = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=45439"
+        self.assertEqual(expected, url_end)
+
+    def test_get_url_for_url_start_count_max_400(self):
+        url1 = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+        url_end = self.puzzle4.get_url_for_url_start_count_max(url1, 400)
+        expected = "http://www.pythonchallenge.com/pc/def/peak.html"
         self.assertEqual(expected, url_end)
 
 if __name__ == "__main__":
