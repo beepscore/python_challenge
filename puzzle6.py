925d7fc375f5ab6a87e311c0412191614254010d
beepscore <support@beepscore.com>
2015-04-14 20:18:42 -0700

    Add puzzle6.py and test_puzzle6.py

---
+++ puzzle6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+class Puzzle6:
+    """
+    test_puzzle5.py explains puzzle6 starting url
+
+    http://www.pythonchallenge.com/pc/def/channel.html
+    """
+
+    def __init__(self):
+        self.puzzle_url = "http://www.pythonchallenge.com/pc/def/channel.html"
---
+++ test/test_puzzle6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import puzzle6
+import unittest
+
+
+class TestPuzzle6(unittest.TestCase):
+
+    def setUp(self):
+        self.puzzle6 = puzzle6.Puzzle6()
+
+
+if __name__ == "__main__":
+    unittest.main()
