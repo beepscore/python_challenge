d0b7fb4003d801ac70f34acae7c3045ab010bbd1
beepscore <support@beepscore.com>
2015-04-25 23:42:49 -0700

    In puzzle6.py docstring add comments about zip.

--- puzzle6.py
+++ puzzle6.py
@@ -6,6 +6,17 @@ class Puzzle6:
     test_puzzle5.py explains puzzle6 starting url
 
     http://www.pythonchallenge.com/pc/def/channel.html
+
+    page shows a partially zipped/unzipped zipper
+    html has a comment?
+    <html> <!-- <-- zip -->
+    Page source tile "now there are pairs"
+    Unzip something?
+
+    http://www.pythonchallenge.com/pc/def/zip.html
+    Says "yes. find the zip".
+    I saved web page to get associated files.
+    I don't see anything in style.css
     """
 
     def __init__(self):
