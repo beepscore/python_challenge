6b12d7ef7fcef21c81ec156a265b12f57b789ad9
beepscore <support@beepscore.com>
2015-04-27 09:02:17 -0700

    In puzzle6.py add comment

--- puzzle6.py
+++ puzzle6.py
@@ -10,6 +10,11 @@ class Puzzle6:
     page shows a partially zipped/unzipped zipper
     html has a comment?
     <html> <!-- <-- zip -->
+    http://www.htmlhelp.com/reference/wilbur/misc/comment.html
+    Is this a deliberately malformed comment?
+    Is zip file somehow hidden or embedded in html or jpg?
+    No, wrong approach.
+
     Page source tile "now there are pairs"
     Unzip something?
 
@@ -17,6 +22,13 @@ class Puzzle6:
     Says "yes. find the zip".
     I saved web page to get associated files.
     I don't see anything in style.css
+
+    looked for a hint
+    http://www.intelligentgeek.com/home/blog/pythonchallenge6
+
+    downloaded
+    http://www.pythonchallenge.com/pc/def/channel.zip
+    then expanded manually, could do it programmatically.
     """
 
     def __init__(self):
